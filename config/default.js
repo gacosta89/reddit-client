e0fd8a37ee54abd8230eb4b74b338f1b0208e60e
Gonzalo Rafael Acosta <gacosta.r89@gmail.com>
2018-09-06 05:00:40 -0300

    Update package.json and config

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ var path = require('path')
 const homeFolder = path.join(__dirname, '../')
 
 module.exports = {
-    name: 'Fancy Hello World',
+    name: 'Reddit Client',
     folders: {
         home: homeFolder,
         client: {
